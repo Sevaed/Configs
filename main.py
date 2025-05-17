955849a2294edca1e54dfe84dabe98fefcee10e9
Sasoed <119231767+Sasoed@users.noreply.github.com>
2025-05-17 13:34:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ with open(CONFIG_PATH, "r") as file:
     data = json.load(file)
 
 
+
+def saveall():
+    subprocess.call([
+        "/home/seva/git/backup/env/bin/python",
+        "/home/seva/git/backup/main.py"
+    ])
+
+
 if len(sys.argv) < 2:
     print("Укажите ключ")
     sys.exit(1)
@@ -19,8 +27,13 @@ arg = sys.argv[1]
 if arg == "-p" and sys.argv[2] in data:
     print(data[sys.argv[2]])
     sys.exit()
+if arg == "-a":
+    print(data)
+    sys.exit()
 if arg in data:
     filepath = data[arg]
     subprocess.call([EDITOR, filepath])
+    saveall()
 else:
     print("Config not found")
+
